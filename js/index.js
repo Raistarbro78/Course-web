5bf4b9c46e359247a388c4f3a0dacaffa3cd9871
saidmalikunknown <safarovsaidmalik@gmail.com>
2025-05-27 00:17:05 +0500

    footer va meadialar qo'shildi

--- js/index.js
+++ js/index.js
@@ -1,16 +1,15 @@
 let menuIcon = document.querySelector(".bi-list");
 let menu = document.querySelector(".header__menu2");
 
-menuIcon.addEventListener("click", function () {
-  if (menu.style.display === "block") {
-    menu.style.display = "none";
-  } else {
-    menu.style.display = "block";
-  }
-});
-let btn = document.querySelector(".lessons__btn")
+// menuIcon.addEventListener("click", function () {
+//   if (menu.style.display === "block") {
+//     menu.style.display = "none";
+//   } else {
+//     menu.style.display = "block";
+//   }
+// });
+// let btn = document.querySelector(".lessons__btn");
 
-btn.addEventListener("click", (el) =>{
-  window.location.href = "./newPage.html"
-})
-
+// btn.addEventListener("click", (el) => {
+//   window.location.href = "./newPage.html";
+// });
